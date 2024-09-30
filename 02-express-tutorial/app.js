31e8dee44f7cdc03f6d84f32752e8f7e651884d7
Satya Kalyan <satyakalyan5@gmail.com>
2024-09-29 22:40:13 -0600

    Learnt Express Routing

--- 02-express-tutorial/app.js
+++ 02-express-tutorial/app.js
@@ -1,33 +1,16 @@
 const express = require("express");
-const path = require("path");
 const app = express();
+const people = require("./routes/people");
+const auth = require("./routes/auth");
 
-//setup static and middleware
-//static asset - file that server does not have to change, left in a specific folder
-//img file, style file, js file
+//static assets
+app.use(express.static("./methods-public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
-const logger = require("./logger.js");
-const authorize = require("./authorize.js");
-app.use([logger, authorize]);
-
-//Here logger is the middleware
-app.get("/", (req, res) => {
-	res.send("Home");
-});
-
-app.get("/about", (req, res) => {
-	res.send("About");
-});
-
-app.all("*", (req, res) => {
-	res.status(404).send("<h1>resource not found</h1>");
-});
+app.use("/api/people", people);
+app.use("/login", auth);
 
 app.listen(6060, () => {
 	console.log("server is listening on port 6060...");
 });
-
-//app.get
-//app.post
-//app.put
-//app.delete
