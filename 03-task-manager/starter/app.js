76b7246338dfb164b507fd19399c1c9d3f6fa53d
Satya Kalyan <satyakalyan5@gmail.com>
2024-10-03 16:26:44 -0600

    Building Task Manager. Updating tasks is pending

---
+++ 03-task-manager/starter/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const app = express();
+const tasks = require("./routes/tasks");
+const connectDB = require("./db/connect");
+require("dotenv").config();
+
+//middleware
+app.use(express.json());
+
+//routes
+app.get("/hello", (req, res) => {
+	res.send("Task Manager App");
+	return;
+});
+
+app.use("/api/v1/tasks", tasks);
+
+const port = 6060;
+const start = async () => {
+	try {
+		await connectDB(process.env.MONGO_URI);
+		app.listen(port, console.log(`Server is listening on port ${port}...`));
+	} catch (err) {
+		console.log(err);
+	}
+};
+
+start();
