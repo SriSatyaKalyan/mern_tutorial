876f09727b0dc1cc80d4a87cbff9908650a417d5
Satya Kalyan <satyakalyan5@gmail.com>
2024-10-10 10:37:34 -0600

    Working on Store API

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,14 +4,19 @@ const tasks = require("./routes/tasks");
 const connectDB = require("./db/connect");
 require("dotenv").config();
 
+const notFound = require("./middleware/not-found");
+const errorHandlerMiddleware = require("./middleware/error-handler");
+
 //middleware
 app.use(express.static("./public"));
 app.use(express.json());
 
 //routes
 app.use("/api/v1/tasks", tasks);
+app.use(notFound);
+app.use(errorHandlerMiddleware);
 
-const port = 6060;
+const port = process.env.PORT || 6060;
 const start = async () => {
 	try {
 		await connectDB(process.env.MONGO_URI);
---
+++ 04-store-api/starter/controllers/products.js
@@ -0,0 +1,16 @@
+const Product = require("../models/product");
+
+const getAllProductsStatic = async (req, res) => {
+	const products = await Product.find({ featured: true });
+	// throw new Error("testing async errors");
+	res.status(200).json({ products, nbHits: products.length });
+};
+
+const getAllProducts = async (req, res) => {
+	res.status(200).json({ msg: "products route" });
+};
+
+module.exports = {
+	getAllProducts,
+	getAllProductsStatic,
+};
