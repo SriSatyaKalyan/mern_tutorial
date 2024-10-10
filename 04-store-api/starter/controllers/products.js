010e56e378341cecb401a75bc60d9348d529bc68
Satya Kalyan <satyakalyan5@gmail.com>
2024-10-10 12:51:01 -0600

    Added sort and fields options to the API

--- 04-store-api/starter/controllers/products.js
+++ 04-store-api/starter/controllers/products.js
@@ -1,13 +1,36 @@
 const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
-	const products = await Product.find({ featured: true });
-	// throw new Error("testing async errors");
+	const products = await Product.find({}).sort("name");
 	res.status(200).json({ products, nbHits: products.length });
 };
 
 const getAllProducts = async (req, res) => {
-	res.status(200).json({ msg: "products route" });
+	const { featured, company, name, sort, fields } = req.query;
+	const queryObject = {};
+
+	if (featured) {
+		queryObject.featured = featured === true ? true : false;
+	}
+
+	//similar to above code for featured except different syntax
+	company ? (queryObject.company = company) : null;
+	name ? (queryObject.name = { $regex: name, $options: "i" }) : null;
+
+	let result = Product.find(queryObject);
+	if (sort) {
+		const sortList = sort.split(",").join(" ");
+		result = result.sort(sortList);
+	} else {
+		result = result.sort("createdAt");
+	}
+
+	const fieldList = fields ? fields.split(",").join(" ") : " ";
+	result = result.select(fieldList);
+
+	const products = await result;
+
+	res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = {
